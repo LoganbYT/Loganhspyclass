15141a66e819ea93cc11265b07a1020559016167
LoganbYT <84545229+LoganbYT@users.noreply.github.com>
2022-10-28 07:57:59 -0500

    Add files via upload

---
+++ Tictacetoe.py
@@ -0,0 +1,46 @@
+from termios import TAB1
+
+
+GameOver = False
+RowX = 0
+RowY = 0
+TacOrTic = 1 
+SkipNum = 1
+TryCount = 0
+
+print("press space bar to start")
+
+
+if #space bar run this:
+    #repeat while GameOver == False:
+    if RowX == 3:
+        print(f"Print congrats character)")
+        GameOver = True
+    elif RowY == 3:
+        print(f"Print congrats + character)")
+        GameOver = True
+
+Funcprint()
+
+Check()
+
+def FuncPrint():
+#Dictionary
+
+def Check():
+    if #numindicTicOrTac == "X":
+        RowX + 1
+        TicOrTac == SkipChangeNum()
+    
+    else:
+        if TryCount = 3
+        RowX = 0
+        SkipChangeNum() 
+
+def SkipChangeNum():
+    if SkipNum == 1:
+        SkipNum = 4:
+
+    
+
+    
