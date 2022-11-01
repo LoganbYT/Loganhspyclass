8d5f967755aa6eec116074d80f735c07c6eff379
LoganbYT <84545229+LoganbYT@users.noreply.github.com>
2022-11-01 08:30:00 -0500

    Add files via upload

--- Tictacetoe.py
+++ Tictacetoe.py
@@ -1,45 +1,92 @@
-from termios import TAB1
-
+import random
+import keyboard
 
 GameOver = False
-RowX = 0
-RowY = 0
-TacOrTic = 1 
-SkipNum = 1
-TryCount = 0
+CorrectTac = 0
+TacOrTic = 1
+Turn = random.uniform(0,1)
+CountToTry = 0
+PlayerInput = 0
+Board = {'1':0, '2':0,'3':0, '4':0, '5':0,'6':0,'7':0,'8':0,'9':0}
+CharacterCheck = "x"
+Winner = "x"
+ActualCheck = 0
 
-print("press space bar to start")
+print("enter P to start")
+while GameOver == False:
+    if keyboard.is_pressed("p"):
+        if Turn == 1:       
+            Board[input("enter your choice as a number:")][1] == "x"
+            Check()
+            if CorrectTac == 3:
+                Turn = 2
+        if Turn == 1:       
+            Board[input("enter your choice as a number:")][1] == "o"
+            Check()
+            if CorrectTac == 3:
+                Turn = 1
 
 
-if #space bar run this:
-    #repeat while GameOver == False:
-    if RowX == 3:
-        print(f"Print congrats character)")
-        GameOver = True
-    elif RowY == 3:
-        print(f"Print congrats + character)")
-        GameOver = True
+    def Check():
+        while ActualCheck != 2:
+            while CountToTry >= 4:
+                if Board[A] & Board[B] & Board[C] == CharacterCheck:
+                    A+3
+                    B+3
+                    C+3 
+                    CorrectTac+1  
+                    CountToTry+1 
+            else:
+                A=1
+                B=4
+                C=7 
+            while CountToTry >= 7:
+                if Board[A] & Board[B] & Board[C] == CharacterCheck:
+                    A+1
+                    B+1
+                    C+1
+                    CorrectTac+1
+                    CountToTry+1
+                else:
+                    A=1
+                    B=5
+                    C=9
+                    if CountToTry >= 8:
+                        if Board[A] & Board[B] & Board[C] == CharacterCheck:
+                        GameOver = True
+                    else:
+                        A=3
+                        B=5
+                        C=7
+                    if CountToTry >= 9:
+                        if Board[A] & Board[B] & Board[C] == CharacterCheck:
+                            GameOver = True
+                            CountToTrys += 1
+                        else:
+                            ActualCheck + 1
+                            CharacterCheck = "o"
+                            CountToTry = 0 
 
-Funcprint()
+                            
 
-Check()
 
-def FuncPrint():
-#Dictionary
+                            
 
-def Check():
-    if #numindicTicOrTac == "X":
-        RowX + 1
-        TicOrTac == SkipChangeNum()
+
+
+        
     
-    else:
-        if TryCount = 3
-        RowX = 0
-        SkipChangeNum() 
+        
+    
 
-def SkipChangeNum():
-    if SkipNum == 1:
-        SkipNum = 4:
+        
+
+              
+                
+
+
+    
+
 
     
 
